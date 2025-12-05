2ee9c22cb8dccd03100b22c0895f502371b561f0
[dyad] <arch.r.soleimani@gmail.com>
2025-12-05 16:47:10 +0100

    Reverted all changes back to version 8330595d151933767362feb6e9db026a1dca1aba

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,45 @@
-"use client";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LcdProvider } from "./context/LcdContext";
+import Layout from "./components/Layout";
+import Menu from "./pages/Menu";
+import ProjectDataSheet from "./pages/ProjectDataSheet";
+import QualitativeEvaluation from "./pages/QualitativeEvaluation";
+import EcoIdeasBoards from "./pages/EcoIdeasBoards";
+import EvaluationChecklists from "./pages/EvaluationChecklists";
+import EvaluationRadar from "./pages/EvaluationRadar";
+import ImprovementRadar from "./pages/ImprovementRadar"; // NEW import
+import NotFound from "./pages/NotFound";
 
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Index from './pages/Index';
-import ProjectDataSheet from './pages/ProjectDataSheet';
-import QualitativeEvaluation from './pages/QualitativeEvaluation';
-import EcoIdeasBoards from './pages/EcoIdeasBoards';
-import EvaluationChecklists from './pages/EvaluationChecklists';
-import EvaluationRadar from './pages/EvaluationRadar';
-import HelpPage from './pages/HelpPage'; // Import the new HelpPage
+const queryClient = new QueryClient();
 
-function App() {
-  return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/project-data" element={<ProjectDataSheet />} />
-          <Route path="/qualitative-evaluation" element={<QualitativeEvaluation />} />
-          <Route path="/eco-ideas" element={<EcoIdeasBoards />} />
-          <Route path="/evaluation-checklists" element={<EvaluationChecklists />} />
-          <Route path="/evaluation-radar" element={<EvaluationRadar />} />
-          <Route path="/help" element={<HelpPage />} /> {/* New route for HelpPage */}
-        </Routes>
-      </Layout>
-    </Router>
-  );
-}
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <LcdProvider>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Menu />} />
+              <Route path="/project-data" element={<ProjectDataSheet />} />
+              <Route path="/qualitative-evaluation" element={<QualitativeEvaluation />} />
+              <Route path="/eco-ideas" element={<EcoIdeasBoards />} />
+              <Route path="/evaluation-checklists" element={<EvaluationChecklists />} />
+              <Route path="/evaluation-radar" element={<EvaluationRadar />} />
+              <Route path="/improvement-radar" element={<ImprovementRadar />} /> {/* NEW route */}
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Layout>
+        </LcdProvider>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App;
