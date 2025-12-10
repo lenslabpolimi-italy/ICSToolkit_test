1e00f4028874071e230fdf611b265c571876577d
[dyad] <arch.r.soleimani@gmail.com>
2025-12-10 13:58:50 +0100

    [dyad] Removed the example link for the 6th guideline of sub-strategy 1.1.1 in Eco-Ideas Boards. - wrote 1 file(s)

--- src/pages/EcoIdeasBoards.tsx
+++ src/pages/EcoIdeasBoards.tsx
@@ -153,7 +153,7 @@ const EcoIdeasBoards: React.FC = () => {
                         const isSecondOverallGuideline = strategyIndex === 0 && subStrategyIndex === 0 && guidelineIndex === 1;
                         const isFourthOverallGuideline = strategyIndex === 0 && subStrategyIndex === 0 && guidelineIndex === 3;
                         const isFifthOverallGuideline = strategyIndex === 0 && subStrategyIndex === 0 && guidelineIndex === 4;
-                        const isSixthOverallGuideline = strategyIndex === 0 && subStrategyIndex === 0 && guidelineIndex === 5;
+                        const isSixthOverallGuideline = strategyIndex === 0 && subStrategyIndex === 0 && guidelineIndex === 5; // NEW condition for 1.1.1.6
                         const isFirstGuidelineOf1_2 = strategyIndex === 0 && subStrategyIndex === 1 && guidelineIndex === 0;
                         const isAvoidPackagingGuideline = guideline.name.toLowerCase().includes('avoid packaging');
                         const isSecondGuidelineOf1_3 = strategyIndex === 0 && subStrategyIndex === 2 && guidelineIndex === 1;
@@ -170,8 +170,8 @@ const EcoIdeasBoards: React.FC = () => {
                           guidelineLink = "https://www.lenslab.polimi.it/wp-content/uploads/2025/07/jeans-IDEA2-english-scaled.png";
                         } else if (isFifthOverallGuideline) {
                           guidelineLink = "https://www.lenslab.polimi.it/wp-content/uploads/2025/07/microstratificata-IDEA2-english-scaled.png";
-                        } else if (isSixthOverallGuideline) {
-                          guidelineLink = "https://www.lenslab.polimi.it/wp-content/uploads/2025/07/tavolo-IDEA2-english-scaled.png";
+                        } else if (isSixthOverallGuideline) { // Apply new link here for 1.1.1.6
+                          guidelineLink = "#"; // Set to '#' to disable the example link
                         } else if (isFirstGuidelineOf1_2) {
                           guidelineLink = "https://www.lenslab.polimi.it/wp-content/uploads/2025/07/staffa-IDEA2-english-scaled.png";
                         } else if (isAvoidPackagingGuideline) {
