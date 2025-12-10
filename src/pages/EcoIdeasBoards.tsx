386c3a1c6bf05c07df036b3484887da9aee5c490
[dyad] <arch.r.soleimani@gmail.com>
2025-12-10 10:15:34 +0100

    [dyad] Added a new example image link for the third guideline of sub-strategy 1.3 in Eco-Ideas Boards. - wrote 1 file(s)

--- src/pages/EcoIdeasBoards.tsx
+++ src/pages/EcoIdeasBoards.tsx
@@ -156,7 +156,8 @@ const EcoIdeasBoards: React.FC = () => {
                         const isSixthOverallGuideline = strategyIndex === 0 && subStrategyIndex === 0 && guidelineIndex === 5;
                         const isFirstGuidelineOf1_2 = strategyIndex === 0 && subStrategyIndex === 1 && guidelineIndex === 0;
                         const isAvoidPackagingGuideline = guideline.name.toLowerCase().includes('avoid packaging');
-                        const isSecondGuidelineOf1_3 = strategyIndex === 0 && subStrategyIndex === 2 && guidelineIndex === 1; // NEW condition
+                        const isSecondGuidelineOf1_3 = strategyIndex === 0 && subStrategyIndex === 2 && guidelineIndex === 1;
+                        const isThirdGuidelineOf1_3 = strategyIndex === 0 && subStrategyIndex === 2 && guidelineIndex === 2; // NEW condition
 
                         let guidelineLink = "#";
                         if (isFirstOverallGuideline) {
@@ -173,8 +174,10 @@ const EcoIdeasBoards: React.FC = () => {
                           guidelineLink = "https://www.lenslab.polimi.it/wp-content/uploads/2025/07/staffa-IDEA2-english-scaled.png";
                         } else if (isAvoidPackagingGuideline) {
                           guidelineLink = "https://www.lenslab.polimi.it/wp-content/uploads/2025/07/imballaggio-IDEA2-english-scaled.png";
-                        } else if (isSecondGuidelineOf1_3) { // Apply new link here
+                        } else if (isSecondGuidelineOf1_3) {
                           guidelineLink = "https://www.lenslab.polimi.it/wp-content/uploads/2025/07/imballaggio2-IDEA2-english-scaled.png";
+                        } else if (isThirdGuidelineOf1_3) { // Apply new link here
+                          guidelineLink = "https://www.lenslab.polimi.it/wp-content/uploads/2025/07/packapplique-english-scaled.png";
                         }
 
                         return (
