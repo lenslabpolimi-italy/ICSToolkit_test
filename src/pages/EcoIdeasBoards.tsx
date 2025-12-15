c51ced538b83c75520ac47c2e3f3e35a77b98872
[dyad] <arch.r.soleimani@gmail.com>
2025-12-15 16:15:37 +0100

    [dyad] Added an example link for guideline 5.1.6. - wrote 1 file(s)

--- src/pages/EcoIdeasBoards.tsx
+++ src/pages/EcoIdeasBoards.tsx
@@ -335,7 +335,8 @@ const EcoIdeasBoards: React.FC = () => {
                             const isGuideline5_1_2 = guidelineId === '5.1.2';
                             const isGuideline5_1_3 = guidelineId === '5.1.3';
                             const isGuideline5_1_4 = guidelineId === '5.1.4';
-                            const isGuideline5_1_5 = guidelineId === '5.1.5'; // NEW
+                            const isGuideline5_1_5 = guidelineId === '5.1.5';
+                            const isGuideline5_1_6 = guidelineId === '5.1.6'; // ADDED
                             const isGuideline5_2_1 = guidelineId === '5.2.1';
                             const isGuideline5_2_2 = guidelineId === '5.2.2';
                             const isGuideline5_2_3 = guidelineId === '5.2.3';
@@ -405,6 +406,8 @@ const EcoIdeasBoards: React.FC = () => {
                               guidelineLink = "https://polimi365-my.sharepoint.com/:i:/g/personal/10004374_polimi_it/IQAt7nKQmYm6R4noAJhTNKWPAehv5s5P2gy1Hh9DGEnBSmE?e=Cy1ooV";
                             } else if (isGuideline5_1_5) {
                               guidelineLink = "https://polimi365-my.sharepoint.com/:i:/g/personal/10004374_polimi_it/IQDdVIeg_cQJT57w3qbXvMedAYXGE8vRVbac77CYAPsU4iI?e=hPL6rv";
+                            } else if (isGuideline5_1_6) {
+                              guidelineLink = "https://polimi365-my.sharepoint.com/:i:/g/personal/10004374_polimi_it/IQDmCPFebfbISayBCzcEkWkXAUWgoAqI2_VPLFqZyDUrK64?e=COG7Qa";
                             }
                             // --- END Specific links ---
 
