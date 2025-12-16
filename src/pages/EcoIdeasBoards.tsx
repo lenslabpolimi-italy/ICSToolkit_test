44d4c9020245bcdf0c56375d67f19323d02b029b
[dyad] <arch.r.soleimani@gmail.com>
2025-12-16 15:45:22 +0100

    [dyad] Moved example link from guideline 7.4.4 to 7.4.5. - wrote 1 file(s)

--- src/pages/EcoIdeasBoards.tsx
+++ src/pages/EcoIdeasBoards.tsx
@@ -602,8 +602,9 @@ const EcoIdeasBoards: React.FC = () => {
                             } else if (isGuideline7_4_2) {
                                 guidelineLink = "https://polimi365-my.sharepoint.com/:i:/g/personal/10004374_polimi_it/IQB637fqdHCTR5eCbRadSeVGAW_lQyPeaW0yjzyyrl4ne20?e=742";
                             } else if (isGuideline7_4_4) {
-                                // Removed specific link for 7.4.4 as requested
                                 guidelineLink = "#";
+                            } else if (isGuideline7_4_5) { // NEW: Link moved from 7.4.4
+                                guidelineLink = "https://polimi365-my.sharepoint.com/:i:/g/personal/10004374_polimi_it/IQB637fqdHCTR5eCbRadSeVGAW_lQyPeaW0yjzyyrl4ne20?e=744";
                             }
                             // --- END Specific links ---
 
@@ -670,12 +671,12 @@ const EcoIdeasBoards: React.FC = () => {
                                 isGuideline6_7_1 || isGuideline6_7_2 || isGuideline6_7_3 ||
                                 // Strategy 6.8 exclusions
                                 isGuideline6_8_1 || isGuideline6_8_2 || isGuideline6_8_3 || isGuideline6_8_4 ||
-                                // NEW: Strategy 7 exclusions (excluding 7.1.6, 7.3.2, 7.4.1, 7.4.2, 7.4.4)
+                                // NEW: Strategy 7 exclusions (excluding 7.1.6, 7.3.2, 7.4.1, 7.4.2, 7.4.5)
                                 isGuideline7_1_1 || isGuideline7_1_2 || isGuideline7_1_3 || isGuideline7_1_4 || isGuideline7_1_5 || isGuideline7_1_7 || isGuideline7_1_8 ||
                                 isGuideline7_1_9 || isGuideline7_1_10 || isGuideline7_1_11 ||
                                 isGuideline7_2_1 || isGuideline7_2_2 || isGuideline7_2_3 || isGuideline7_2_4 || isGuideline7_2_5 ||
                                 isGuideline7_3_1 || isGuideline7_3_3 || isGuideline7_3_4 || isGuideline7_3_5 || isGuideline7_3_6 ||
-                                isGuideline7_4_3 || isGuideline7_4_5 || isGuideline7_4_6 || isGuideline7_4_7 || isGuideline7_4_8 || isGuideline7_4_9 ||
+                                isGuideline7_4_3 || isGuideline7_4_4 || isGuideline7_4_6 || isGuideline7_4_7 || isGuideline7_4_8 || isGuideline7_4_9 ||
                                 isGuideline7_5_1 || isGuideline7_5_2 || isGuideline7_5_3 || isGuideline7_5_4 || isGuideline7_5_5 || isGuideline7_5_6;
 
                             return (
